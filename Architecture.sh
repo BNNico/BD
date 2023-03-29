667e9edb69dcfbc61118a58d3655281d0c2099aa
Nicobaharaye Brice-Nathan <nafanicobaharaye@gmail.com>
2023-03-29 19:58:12 +0200

    .sh pour changement archi

---
+++ Architecture.sh
@@ -0,0 +1,46 @@
+mkdir Entrainement
+mkdir Test
+mkdir Validation
+cd Entrainement
+mkdir CDM_2014
+mkdir CDM_2018
+mkdir CDM_2022
+mkdir EURO_2016
+mkdir EURO_2020
+cd ../Test
+mkdir CDM_2014
+mkdir CDM_2018
+mkdir CDM_2022
+mkdir EURO_2016
+mkdir EURO_2020
+cd ../Validation
+mkdir CDM_2014
+mkdir CDM_2018
+mkdir CDM_2022
+mkdir EURO_2016
+mkdir EURO_2020
+# CDM_2014
+cd ../CDM_2014/Entrainement
+cp *.jpg ../../Entrainement/CDM_2014
+cd ../Test
+cp *.jpg ../../Test/CDM_2014
+# CDM_2018
+cd ../../CDM_2018/Entrainement
+cp *.jpg ../../Entrainement/CDM_2018
+cd ../Test
+cp *.jpg ../../Test/CDM_2018
+# CDM_2022
+cd ../../CDM_2022/Entrainement
+cp *.jpg ../../Entrainement/CDM_2022
+cd ../Test
+cp *.jpg ../../Test/CDM_2022
+# EURO_2016
+cd ../../EURO_2016/Entrainement
+cp *.jpg ../../Entrainement/EURO_2016
+cd ../Test
+cp *.jpg ../../Test/EURO_2016
+# EURO_2020
+cd ../../EURO_2020/Entrainement
+cp *.jpg ../../Entrainement/EURO_2020
+cd ../Test
+cp *.jpg ../../Test/EURO_2020
