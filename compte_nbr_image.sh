3977f1c9e98bbc12103ed24691ec8f2938b42304
Louis Jovenet <95980588+LouisJovenet@users.noreply.github.com>
2023-03-29 11:09:53 +0200

    update entrainement/test

--- compte_nbr_image.sh
+++ compte_nbr_image.sh
@@ -3,6 +3,8 @@
 # Boucle pour parcourir tous les dossiers du répertoire courant
 for directory in */; do
     # Compte le nombre de fichiers dans le dossier courant et l'affiche
-    num_files=$(ls -1 $directory | wc -l)
-    echo "Le dossier $directory contient $num_files images."
+    num_files=$(ls -1 $directory/Entrainement | wc -l)
+    echo "Le dossier $directory/Entrainement contient $num_files images."
+    num_files=$(ls -1 $directory/Test | wc -l)
+    echo "Le dossier $directory/Test contient $num_files images."
 done
