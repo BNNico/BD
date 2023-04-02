e393811f391e8b1b12a47df27d65cf63bb1db60c
Nicobaharaye Brice-Nathan <nafanicobaharaye@gmail.com>
2023-04-02 13:19:08 +0200

    push qui sert a rien j'ai juste chnager le code d'architecture

--- Architecture.sh
+++ Architecture.sh
@@ -1,46 +1,12 @@
-mkdir Entrainement
-mkdir Test
-mkdir Validation
-cd Entrainement
-mkdir CDM_2014
-mkdir CDM_2018
-mkdir CDM_2022
-mkdir EURO_2016
-mkdir EURO_2020
-cd ../Test
-mkdir CDM_2014
-mkdir CDM_2018
-mkdir CDM_2022
-mkdir EURO_2016
-mkdir EURO_2020
-cd ../Validation
-mkdir CDM_2014
-mkdir CDM_2018
-mkdir CDM_2022
-mkdir EURO_2016
-mkdir EURO_2020
-# CDM_2014
-cd ../CDM_2014/Entrainement
-cp *.jpg ../../Entrainement/CDM_2014
-cd ../Test
-cp *.jpg ../../Test/CDM_2014
-# CDM_2018
-cd ../../CDM_2018/Entrainement
-cp *.jpg ../../Entrainement/CDM_2018
-cd ../Test
-cp *.jpg ../../Test/CDM_2018
-# CDM_2022
-cd ../../CDM_2022/Entrainement
-cp *.jpg ../../Entrainement/CDM_2022
-cd ../Test
-cp *.jpg ../../Test/CDM_2022
-# EURO_2016
-cd ../../EURO_2016/Entrainement
-cp *.jpg ../../Entrainement/EURO_2016
-cd ../Test
-cp *.jpg ../../Test/EURO_2016
-# EURO_2020
-cd ../../EURO_2020/Entrainement
-cp *.jpg ../../Entrainement/EURO_2020
-cd ../Test
-cp *.jpg ../../Test/EURO_2020
+# Création des répertoires Entrainement, Test et Validation
+mkdir -p {Entrainement,Test,Validation}
+
+# Création des sous-répertoires pour chaque compétition et chaque ensemble
+for comp in CDM_2014 CDM_2018 CDM_2022 EURO_2016 EURO_2020; do
+  mkdir -p Entrainement/$comp Test/$comp Validation/$comp
+done
+
+# Copie des fichiers JPG dans les sous-répertoires correspondants
+for comp in CDM_2014 CDM_2018 CDM_2022 EURO_2016 EURO_2020; do
+  cp $comp/*.jpg Entrainement/$comp Test/$comp
+done
